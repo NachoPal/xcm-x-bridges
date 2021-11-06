773bdbc697b06e9b37f36ec124b36d83f4fd5293
NachoPal <ignacio.palacios.santos@gmail.com>
2021-11-06 20:18:52 +0100

    dmp & ump teleport assets work

--- scripts/run/init-chains.sh
+++ scripts/run/init-chains.sh
@@ -8,12 +8,25 @@ then
 fi  
 
 # ###############################################################################
-# ### Init Chains #######################################################
+# ### Init Relay Chains #######################################################
 # ###############################################################################
-echo "Init Chains"
+echo "Init Relay Chains"
 
 yarn "$dev"init-chains\
+  -c relay\
   -i $PARA_ID_SOURCE\
   -p $PORT_SOURCE\
   -u //Alice \
-  -v 2 &> ./logs/init-chains.log&
+  -v 2 &> ./logs/init-relay-chains.log&
+
+# ###############################################################################
+# ### Init ParaChains #######################################################
+# ###############################################################################
+echo "Init ParaChains"
+
+yarn "$dev"init-chains\
+  -c para\
+  -i $PARA_ID_SOURCE\
+  -p $PORT_PARA_SOURCE\
+  -u //Alice \
+  -v 2 &> ./logs/init-para-chains.log&
--- src/initChains.ts
+++ src/initChains.ts
@@ -1,63 +1,50 @@
-// import fs from 'fs';
 import commandLineArgs from 'command-line-args';
 import connectToRelayChains from './common/connectToRelayChains';
 import getWallet from './common/getWallet';
 
 
-const forceXcmVersion = async ({ api, uri, id, version }) => {
-  const wallet = await getWallet(uri);
-
-  const location = { parent: 0, interior: { x1: { parachain: id }}}
+const forceXcmVersion = async ({ api, location, version, wallet, pallet }) => {
   let nonce = await api.rpc.system.accountNextIndex(wallet.address);
   await api.tx.sudo
-			.sudo(api.tx.xcmPallet.forceXcmVersion(location, version))
+			.sudo(api.tx[pallet].forceXcmVersion(location, version))
 			.signAndSend(wallet, { nonce, era: 0 });
 }
 
-// const setupParachain = async (relayChain) => {
-//   const { uri, api, genesis, wasm, id } = relayChain;
-//   const wallet = await getWallet(uri);
-//   await reserveParachainId(api, wallet);
-
-//   const { 
-//     genesis: genesisHex, 
-//     wasm: wasmHex 
-//   } = getGenesisAndWasm(genesis, wasm);
-
-//   const paraGenesisArgs = {
-//     genesis_head: genesisHex,
-//     validation_code: wasmHex,
-//     parachain: true,
-//   };
-//   let genesisArgs = api.createType("ParaGenesisArgs", paraGenesisArgs);
-
-//   await registerParachain(api, wallet, id, genesisArgs);
-// }
+const forceDefaultXcmVersion = async ({ api, version, wallet, pallet }) => {
+  let nonce = await api.rpc.system.accountNextIndex(wallet.address);
+  await api.tx.sudo
+			.sudo(api.tx[pallet].forceDefaultXcmVersion(version))
+			.signAndSend(wallet, { nonce, era: 0 });
+}
 
 const main = async () => {
   const optionDefinitions = [
+    { name: 'chain', alias: 'c', type: String },
     { name: 'port', alias: 'p', type: String },
     { name: 'uri', alias: 'u', type: String },
     { name: 'id', alias: 'i', type: Number },
     { name: 'version', alias: 'v', type: Number}
   ]
   const options = commandLineArgs(optionDefinitions);
-  const { port, id, version, uri } = options;
+  const { chain, port, id, version, uri } = options;
 
   const relayChain = await connectToRelayChains(port, undefined);
 
+  let location = chain === 'relay' ? 
+    { parent: 0, interior: { x1: { parachain: id }}} : { parent: 1, interior: { here: true }}
+  
+  let pallet = chain === 'relay' ? 'xcmPallet' : 'polkadotXcm'
+
   const data = { // source
     api: relayChain.source.chain.api,
-    uri,
-    id,
+    location,
     version,
+    wallet: await getWallet(uri),
+    pallet
   }
 
-  // let parachainPromises: Array<Promise<void>> = [];
-
+  await forceDefaultXcmVersion(data)  
   await forceXcmVersion(data)
-
-  // Promise.all(parachainPromises)
 }
 
 main()
